aed5acfc42d8273ad65fab8fef93046c9969826e
AlexScotte <scottealexandre@gmail.com>
2023-07-25 21:58:00 +0200

    [challenge 2] checkpoint 3

--- challenge-2-token-vendor/packages/hardhat-ts/contracts/Vendor.sol
+++ challenge-2-token-vendor/packages/hardhat-ts/contracts/Vendor.sol
@@ -1,19 +1,35 @@
 pragma solidity >=0.8.0 <0.9.0;
 // SPDX-License-Identifier: MIT
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/access/Ownable.sol';
 import './YourToken.sol';
 
-contract Vendor {
+import 'hardhat/console.sol';
+
+contract Vendor is Ownable {
   YourToken public yourToken;
 
-  constructor(address tokenAddress) public {
+  uint256 public constant tokensPerEth = 100;
+  event BuyTokens(address buyer, uint256 amountOfEth, uint256 amountOfTokens);
+
+  constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
 
   // ToDo: create a payable buyTokens() function:
+  function buyTokens() public payable {
+    uint256 amountOfTokens = msg.value * tokensPerEth;
+    yourToken.transfer(msg.sender, msg.value);
+    emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+  }
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  function withdraw(uint256 amountOfToken) public onlyOwner {
+    console.log(yourToken.balanceOf(owner()));
+    require(yourToken.balanceOf(owner()) >= amountOfToken, 'not enough token in the contract');
+
+    yourToken.transfer(owner(), amountOfToken);
+  }
 
   // ToDo: create a sellTokens() function:
 }
--- challenge-2-token-vendor/packages/hardhat-ts/deploy/01_deploy_vendor.ts
+++ challenge-2-token-vendor/packages/hardhat-ts/deploy/01_deploy_vendor.ts
@@ -13,24 +13,25 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironmentExtended) => {
 
   // Todo: deploy the vendor
 
-  // await deploy('Vendor', {
-  //   // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
-  //   from: deployer,
-  //   args: [yourToken.address],
-  //   log: true,
-  // });
+  await deploy('Vendor', {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    args: [yourToken.address],
+    log: true,
+  });
 
-  // const vendor = await ethers.getContract("Vendor", deployer);
+  const vendor = await ethers.getContract("Vendor", deployer);
 
   // Todo: transfer the tokens to the vendor
-  // console.log("\n 🏵  Sending all 1000 tokens to the vendor...\n");
+  console.log("\n 🏵  Sending all 1000 tokens to the vendor...\n");
 
-  // await yourToken.transfer(
-  //   vendor.address,
-  //   ethers.utils.parseEther("1000")
-  // );
+  var tx = await yourToken.transfer(
+    vendor.address,
+    ethers.utils.parseEther("1000")
+  );
+  await tx.wait();
 
-  // await vendor.transferOwnership("**YOUR FRONTEND ADDRESS**");
+  await vendor.transferOwnership("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
 };
 export default func;
 func.tags = ['Vendor'];
